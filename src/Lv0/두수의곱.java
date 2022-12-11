b4bb652baf2993f9c47b35181ed99384a479f242
HOng <moonshin123@naver.com>
2022-12-11 16:27:06 +0900

    두수의 곱

---
+++ src/Lv0/두수의곱.java
@@ -0,0 +1,11 @@
+package Lv0;
+
+public class 두수의곱 {
+    public static void main(String[] args) {
+        System.out.println(solution(2, 3));
+    }
+
+    static int solution(int num1, int num2) {
+        return num1 * num2;
+    }
+}
